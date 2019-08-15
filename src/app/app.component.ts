bf5d42b5c0da268aedfdbd899501bdd3fad9d758
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2019-08-14 21:24:46 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre = 'Marco Antonio';
+  nombre2 = 'susAn villarreal gutiérrez';
+  array = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('llegó la data'), 3500);
+  });
+  fecha = new Date();
+  video = 'XaiNzHKm1Fk';
+  activar:boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
